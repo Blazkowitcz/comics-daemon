3c6fa91749d0c92a31439fcc6bd29e8bd62293a2
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-03-26 13:09:06 +0100

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "mocha": true,
+        "es6": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module",
+        "parser": "babel-eslint"
+    },
+    "rules": {
+        "eqeqeq": "error",
+        "curly": "error",
+        "semi": "error",
+        "no-console": "error",
+        "require-jsdoc": "error"
+    },
+};
---
+++ app/routes/library.route.js
@@ -0,0 +1,5 @@
+module.exports = app => {
+    const library = require('../controllers/library.controller');
+
+    app.get('/libraries', )
+}
