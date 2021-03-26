ea4c18cec83e0d795024bc76272ebf08f18f8eb1
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-03-27 00:01:04 +0100

    Add body-parser module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 require("./app/routes/library.route")(app);
 
 app.listen(3000, function () {});
